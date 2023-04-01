89ba92f57289c16fe38f1b139c38481430581c78
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-04-01 18:43:48 +0800

    fix: turn off `unicorn/prefer-top-level-await`

--- src/overrides/unicorn.js
+++ src/overrides/unicorn.js
@@ -1,6 +1,7 @@
 /** @type { import('eslint').Linter.RulesRecord } */
 export const unicornRules = {
   'unicorn/prefer-export-from': ['error', { ignoreUsedVariables: true }],
+  'unicorn/prefer-top-level-await': 'off',
   'unicorn/prevent-abbreviations': 'off',
   'unicorn/switch-case-braces': ['error', 'avoid'],
 }
