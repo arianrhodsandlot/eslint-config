78b61379699fb03033de53d94491d4d5d4333a74
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-04-20 16:05:39 +0800

    fix: turn off unicorn/no-useless-undefined's  option

--- src/overrides/unicorn.js
+++ src/overrides/unicorn.js
@@ -3,5 +3,6 @@ export const unicornRules = {
   'unicorn/prefer-export-from': ['error', { ignoreUsedVariables: true }],
   'unicorn/prefer-top-level-await': 'off',
   'unicorn/prevent-abbreviations': 'off',
+  'unicorn/no-useless-undefined': ['error', { checkArguments: false }],
   'unicorn/switch-case-braces': ['error', 'avoid'],
 }
