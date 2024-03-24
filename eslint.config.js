05205c554c46fafaaac74d739b32fe3352fc1e56
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-03-24 15:37:49 +0800

    fix: fix ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,5 @@
-import { createConfig as createNormalConfig } from '@arianrhodsandlot/eslint-config'
+import { createConfig } from '@arianrhodsandlot/eslint-config'
 
-let createDebugConfig
-try {
-  const mod = await import('./dist/esm/index.js')
-  createDebugConfig = mod.createConfig
-} catch {}
-
-const isDebug = false
-const createConfig = isDebug ? createDebugConfig : createNormalConfig
-
-// @ts-expect-error only has error when debug
 export default createConfig({
   append: [{ rules: { '@typescript-eslint/ban-ts-comment': 'off' } }],
 })
