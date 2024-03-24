1005f4a490e7885bf1476f395b2d72fd91937b60
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-03-24 15:45:02 +0800

    fix: fix ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,1 @@
-import { createConfig } from '@arianrhodsandlot/eslint-config'
-
-export default createConfig({
-  append: [{ rules: { '@typescript-eslint/ban-ts-comment': 'off' } }],
-})
+export default {}
