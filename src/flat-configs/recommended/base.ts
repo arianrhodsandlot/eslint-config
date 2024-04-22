f21d2bcf3fe796594605e3ce4373b0c49ff0331d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-04-22 18:16:57 +0800

    fix: add global parser

--- src/flat-configs/recommended/base.ts
+++ src/flat-configs/recommended/base.ts
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import { jsOrTsGlob } from '../../lib/constants.js'
 import { getGitIgnores } from '../../lib/utils.js'
 import type { FlatConfigs } from '../../types/eslint.js'
@@ -7,6 +8,10 @@ export const baseConfigs: FlatConfigs = [
   {
     files: [jsOrTsGlob],
     ignores: getGitIgnores(),
+    languageOptions: {
+      // @ts-expect-error types in tseslint do not match those in eslint
+      parser: tseslint.parser,
+    },
     linterOptions: { reportUnusedDisableDirectives: true },
   },
   js.configs.recommended,
