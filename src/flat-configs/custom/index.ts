2d67df4541c2facfb5ace7191a05ed5b3b17c1fd
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-04-26 16:41:16 +0800

    fix: add missing n custom configs

--- src/flat-configs/custom/index.ts
+++ src/flat-configs/custom/index.ts
@@ -4,16 +4,20 @@ import { getImportConfigs } from './import.js'
 import { getJsConfigs } from './js.js'
 import { getJsdocConfigs } from './jsdoc.js'
 import { getMarkdownConfigs } from './markdown.js'
+import { getNConfigs } from './n.js'
 import { getTestConfigs } from './test.js'
 
 export function getCustomFlatConfigs() {
   return [
     ...getCustomRulesConfigs(),
+
     ...getConflictConfigs(),
+
     ...getImportConfigs(),
     ...getJsConfigs(),
     ...getJsdocConfigs(),
     ...getMarkdownConfigs(),
+    ...getNConfigs(),
     ...getTestConfigs(),
   ]
 }
--- src/flat-configs/custom/n.ts
+++ src/flat-configs/custom/n.ts
@@ -1,14 +1,17 @@
 import _ from 'lodash'
 import { jsxOrTsxGlob } from '../../lib/constants.js'
 import { eslintPluginN } from '../../lib/eslint-plugins.js'
+import type { FlatConfigs } from '../../types/eslint.js'
 
 const { mapValues } = _
 
 export function getNConfigs() {
-  return [
+  const nConfigs: FlatConfigs = [
     {
       files: [jsxOrTsxGlob, '**/*.vue'],
       rules: mapValues(eslintPluginN.configs['flat/recommended'].rules, () => 'off'),
     },
   ]
+
+  return nConfigs
 }
