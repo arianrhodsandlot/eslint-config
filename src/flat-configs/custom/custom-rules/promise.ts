faf0e2bdeef039c0a6163d70f8858b08ddbd1612
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-04-28 13:48:34 +0800

    fix: update promise/always-return

--- src/flat-configs/custom/custom-rules/promise.ts
+++ src/flat-configs/custom/custom-rules/promise.ts
@@ -1,6 +1,7 @@
 import type { FlatConfigRules } from '../../../types/eslint.js'
 
 export const promiseRules: FlatConfigRules = {
+  'promise/always-return': ['error', { ignoreLastCallback: true }],
   'promise/no-multiple-resolved': 'error',
   'promise/prefer-await-to-then': 'error',
 }
