9d3e2d9acab24a8da20193d14cde6136cf16aee2
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2024-05-31 13:28:19 +0800

    fix: do not enable yaml parser

--- src/flat-configs/recommended/vue-i18n.ts
+++ src/flat-configs/recommended/vue-i18n.ts
@@ -2,5 +2,6 @@ import { eslintPluginVueI18n } from '../../lib/eslint-plugins.js'
 import type { FlatConfigs } from '../../types/eslint.js'
 
 export const vueI18nConfigs: FlatConfigs = eslintPluginVueI18n.configs['flat/recommended'].filter(
-  ({ name }: { name: string }) => name !== '@intlify/vue-i18n:base:setup:json',
+  ({ name }: { name: string }) =>
+    !['@intlify/vue-i18n:base:setup:json', '@intlify/vue-i18n:base:setup:yaml'].includes(name),
 )
