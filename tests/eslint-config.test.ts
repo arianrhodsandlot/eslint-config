b3fcd8b5b1806e0551b1ca74212536572cc65e65
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-01-15 16:33:40 +0800

    test: migrate to the built in snapshots feature of nodejs

--- tests/eslint-config.test.ts
+++ tests/eslint-config.test.ts
@@ -1,5 +1,3 @@
-import { deepEqual } from 'node:assert'
-import { readFileSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
 import { describe, test } from 'node:test'
 import { fileURLToPath } from 'node:url'
@@ -7,7 +5,6 @@ import { loadESLint } from 'eslint'
 
 const testsDir = path.dirname(fileURLToPath(import.meta.url))
 const inputsDir = path.join(testsDir, 'inputs')
-const messagesDir = path.join(testsDir, 'messages')
 
 describe('ESLint config', async () => {
   const TestESLint = await loadESLint()
@@ -23,14 +20,10 @@ describe('ESLint config', async () => {
     'test-vue.vue',
   ]
   for (const inputFile of inputFiles) {
-    await test(inputFile, async () => {
-      const messagesFileName = path.format({ ...path.parse(inputFile), base: undefined, ext: '.json' })
-      const messagesPath = path.join(messagesDir, messagesFileName)
-      const expectedMessages = JSON.parse(readFileSync(messagesPath, 'utf8'))
+    await test(inputFile, async (t) => {
       const file = path.join(inputsDir, inputFile)
       const [{ messages }] = await eslint.lintFiles(file)
-      writeFileSync(messagesPath, JSON.stringify(messages, null, 2))
-      deepEqual(messages, expectedMessages)
+      t.assert.snapshot(messages)
     })
   }
 })
