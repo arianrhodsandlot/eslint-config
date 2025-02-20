65a8ccbf0adc052e49f3e6d911a290d46f6d26b9
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-02-20 17:27:04 +0800

    test: update test snapshots

--- src/flat-configs/custom/custom-rules/stylistic.ts
+++ src/flat-configs/custom/custom-rules/stylistic.ts
@@ -1,6 +1,5 @@
 import type { FlatConfigRules } from '../../../types/eslint.ts'
 
 export const stylisticRules: FlatConfigRules = {
-  '@stylistic/jsx-quotes': ['error', 'prefer-single'],
   '@stylistic/jsx-self-closing-comp': ['error', { component: true, html: true }],
 }
--- src/flat-configs/recommended/stylistic.ts
+++ src/flat-configs/recommended/stylistic.ts
@@ -1,4 +1,9 @@
 import { eslintPluginStylistic } from '../../lib/eslint-plugins.js'
 import type { FlatConfigs } from '../../types/eslint.js'
 
-export const stylisticConfigs: FlatConfigs = [eslintPluginStylistic.configs.recommended]
+export const stylisticConfigs: FlatConfigs = [
+  {
+    ...eslintPluginStylistic.configs.recommended,
+    rules: {},
+  },
+]
