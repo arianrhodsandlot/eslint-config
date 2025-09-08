234d1365d2e81fbd63a8a0ca04593f9da0a0f1e3
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-09-08 10:09:40 +0800

    feat: enable tailwind options for the css plugin

--- src/flat-configs/custom/tailwind.ts
+++ src/flat-configs/custom/tailwind.ts
@@ -1,17 +1,15 @@
 import type { FlatConfigs } from '../../types/eslint.ts'
 
 export function getTailwindConfigs() {
-  const typeScriptConfigs: FlatConfigs = [
+  const tailwindConfigs: FlatConfigs = [
     {
       name: 'tailwind',
       settings: {
         tailwindcss: {
           callees: ['cc', 'classcat', 'classnames', 'clsx', 'cn', 'ctl', 'cva', 'tv', 'twJoin', 'twMerge'],
-          classRegex: 'class',
-          tags: ['tw'],
         },
       },
     },
   ]
-  return typeScriptConfigs
+  return tailwindConfigs
 }
--- src/flat-configs/recommended/css.ts
+++ src/flat-configs/recommended/css.ts
@@ -1,10 +1,14 @@
 import eslintPluginCss from '@eslint/css'
+import { tailwind4 } from 'tailwind-csstree'
 import type { FlatConfigs } from '../../types/eslint.js'
 
 export const cssConfigs: FlatConfigs = [
   {
     files: ['**/*.css'],
     language: 'css/css',
+    languageOptions: {
+      customSyntax: tailwind4,
+    },
     name: 'css/css',
     ...eslintPluginCss.configs.recommended,
   },
