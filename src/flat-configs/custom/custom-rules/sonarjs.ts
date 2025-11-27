37c73b9fb56748b06443cac4bffea76c119c0a8f
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-11-28 00:08:22 +0800

    feat: esm only

--- src/flat-configs/custom/custom-rules/sonarjs.ts
+++ src/flat-configs/custom/custom-rules/sonarjs.ts
@@ -3,6 +3,7 @@ import type { FlatConfigRules } from '../../../types/eslint.js'
 export const sonarjsRules: FlatConfigRules = {
   'sonarjs/code-eval': 'off',
   'sonarjs/cognitive-complexity': 'error',
+  'sonarjs/hashing': 'off',
   'sonarjs/no-duplicate-string': ['error', { threshold: 20 }],
   'sonarjs/no-hardcoded-passwords': 'off',
   'sonarjs/no-os-command-from-path': 'off',
