c563da4a93c132d6b27c33d4ac453b679380c3be
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-12-27 21:32:02 +0800

    fix: fix deps

--- src/flat-configs/recommended/perfectionist.ts
+++ src/flat-configs/recommended/perfectionist.ts
@@ -2,5 +2,6 @@ import perfectionist from 'eslint-plugin-perfectionist'
 import type { FlatConfigs } from '../../types/eslint.js'
 
 export const perfectionistConfigs: FlatConfigs = [
+  // @ts-expect-error perfectionist's configs type is not compatible with FlatConfigs
   { name: 'perfectionist/recommended', ...perfectionist.configs['recommended-alphabetical'] },
 ]
